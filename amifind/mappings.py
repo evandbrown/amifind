2320f8aa4ed6306de6c9f66d0f9843b88c0aa43a
Evan Brown <evandbrown@gmail.com>
2013-06-13 22:11:52 -0700

    Move mappings to separate module

---
+++ amifind/mappings.py
@@ -0,0 +1,37 @@
+"""
+dicts that maps EC2 API filter values to more concise names (e.g., x86_64 -> 64)
+"""
+
+operating_systems = {
+    'Windows': 'windows',
+    'windows': 'windows',
+    'linux': 'linux'
+}
+""" Supported values for os filter """
+
+architectures = {
+    '32': 'i386',
+    'i386': 'i386',
+    '64': 'x86_64',
+    'x86_64': 'x86_64'
+}
+""" Supported values for arch filter"""
+
+virtualization_types = {
+    'hvm': 'hvm',
+    'pv': 'paravirtual',
+    'paravirtual': 'paravirtual'
+}
+""" Supported values for virt_type filter """
+
+root_device_types = {
+    'ebs': 'ebs',
+    's3': 'instance-store',
+    'instance-store': 'instance-store'
+}
+
+just_one = {
+    'first' : 0,
+    'last' : -1
+}
+""" Supported values for root_dev_type """
